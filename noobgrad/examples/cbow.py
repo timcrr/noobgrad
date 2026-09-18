fb4077be2fbd3a4d5782ccb47c13c17b65b70b3d
yarullint <y4rullin4@gmail.com>
2026-09-18 22:19:17 +0300

    train val test pipeline

--- noobgrad/examples/cbow.py
+++ noobgrad/examples/cbow.py
@@ -1,14 +1,19 @@
+import torch
 import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import TensorDataset, DataLoader
 import re
 import csv
+from tqdm.auto import tqdm
 from datasets import load_dataset
 
+def trange(n): return tqdm(n, total=len(n))
+
 class CBOW(nn.Module):
-  def __init__(self, voc_size, embed_dim, window_size):
+  def __init__(self, voc_size, embed_dim):
     super().__init__()
     self.ff1 = nn.Embedding(voc_size, embed_dim) # support only word ids
     self.ff2 = nn.Linear(embed_dim, voc_size)
-    self.window_size = window_size
   def forward(self, x):
     # x: (B, C, V)
     x = self.ff1(x) # (B, C, E)
@@ -41,24 +46,94 @@ def create_cbow_pairs(sentence, voc, window_size):
     pairs.append((context, target))
   return pairs
 
-def main(dset_path: str, output_file: str, window_size: int):
+def main(dset_path: str, dset_split: str, output_file: str, window_size: int, delimiter: str=','):
   """
   Load basic dataset, than based on that building vocabulary and cbow pairs
   """
   ds = load_dataset(dset_path)
-  train = ds["train"]
-  texts = train["text"]
-  voc = build_vocabulary(texts)
+  train_split = ds['train']
+  diff_split = ds[dset_split]
+  texts = diff_split["text"]
+  voc = build_vocabulary(train_split["text"]) # voc from train split
 
   with open(output_file, 'w', newline='', encoding='utf-8') as file:
-    writer = csv.writer(file, delimiter=',')
+    writer = csv.writer(file, delimiter=delimiter)
     writer.writerow(['context', 'target'])
     for text in texts:
       pairs = create_cbow_pairs(text, voc, window_size)
       for context, target in pairs:
         writer.writerow([" ".join(map(str, context)), target])
+  return len(voc)
+
+def get_tensorset(csv_file: str, limit: int = -1, delimiter: str=',') -> DataLoader:
+  contexts = []
+  targets = []
+  with open(csv_file, 'r', encoding='utf-8') as file:
+    reader = csv.DictReader(file, delimiter=delimiter)
+    for row in reader:
+      context = list(map(int, row['context'].split()))
+      target = int(row['target'])
+      contexts.append(context)
+      targets.append(target)
+
+  contexts = torch.tensor(contexts, dtype=torch.long)
+  targets = torch.tensor(targets, dtype=torch.long)
+
+  dataset = TensorDataset(contexts, targets)
+  if limit != -1:
+    dataset = torch.utils.data.Subset(dataset, range(limit))
+  dataloader = DataLoader(dataset, batch_size=64, shuffle=True)
+  return dataloader
+
+def validate(model, criterion, loader, device):
+  model.eval()
+  for X, y in (t:=trange(loader)):
+    X, y = X.to(device), y.to(device)
+    logits = model(X)
+    loss = criterion(logits, y)
+    t.set_description(f"***  validation loss: {loss.item():.4f}")
+
+def test(model, loader, device):
+  model.eval()
+  total_params = sum(p.numel() for p in model.parameters())
+  print(total_params)
+  total_correct = 0
+  total_n = 0
+  for X, y in tqdm(loader):
+    X, y = X.to(device), y.to(device)
+    logits = model(X)
+    _, preds = torch.max(logits, 1)
+    total_correct += (preds == y).sum().item()
+    total_n += y.size(0)
+  accuracy = total_correct / total_n
+  return accuracy
 
 if __name__ == "__main__":
-  dset_path = "/Users/timcr/.cache/huggingface/hub/datasets--Salesforce--wikitext"
-  output_file = "output.csv"
-  main(dset_path, output_file, window_size=3)
+  '''
+  dset_path = '/Users/timcr/.cache/huggingface/hub/datasets--Salesforce--wikitext'
+  output_file = 'data/wiki_cbow/test.csv'
+  dset_split = 'test'
+  voc_size = main(dset_path, dset_split, output_file, window_size=3)
+  print(voc_size) # 62300
+  '''
+  device = torch.accelerator.current_accelerator().type
+  trainloader = get_tensorset(csv_file='data/wiki_cbow/train.csv')
+  valloader = get_tensorset(csv_file='data/wiki_cbow/validation.csv')
+  testloader = get_tensorset(csv_file='data/wiki_cbow/test.csv')
+  model = CBOW(62300, 300).to(device)
+  criterion = nn.CrossEntropyLoss()
+  optimizer = optim.Adam(model.parameters(), lr=1e-3)
+  epochs = 5
+  print(f"***  using device:  {device}")
+  for epoch in range(epochs):
+    for X, y in (t:=trange(trainloader)):
+      X, y = X.to(device), y.to(device)
+      logits = model(X)
+      loss = criterion(logits, y)
+      optimizer.zero_grad()
+      loss.backward()
+      optimizer.step()
+      t.set_description(f"***  [{epoch+1}/{epochs}]   loss: {loss.item():.4f}")
+    validate(model, criterion, valloader, device)
+  accuracy = test(model, testloader, device)
+  print(f"***  test accuracy: {accuracy:.4f}")
