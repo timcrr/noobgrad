123b0a599e843175dbc88845a4cf79e681ab18aa
yarullint <y4rullin4@gmail.com>
2026-09-25 22:33:30 +0300

    add causal masking for attn

--- noobgrad/torchexamples/mhead_attn.py
+++ noobgrad/torchexamples/mhead_attn.py
@@ -3,8 +3,10 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+device = torch.accelerator.current_accelerator().type
+
 class MultiHeadAttention(nn.Module):
-  def __init__(self, embed_dim, num_heads):
+  def __init__(self, embed_dim, num_heads, causal:bool=False):
     super().__init__()
     if embed_dim % num_heads != 0: raise ValueError("embed dim should be divisible by num heads")
     self.embed_dim = embed_dim
@@ -15,6 +17,8 @@ class MultiHeadAttention(nn.Module):
     self.k_proj = nn.Linear(embed_dim,embed_dim,bias=False)
     self.v_proj = nn.Linear(embed_dim,embed_dim,bias=False)
     self.out_proj = nn.Linear(embed_dim,embed_dim,bias=False)
+
+    self.causal = causal
   def _split_heads(self,x,bs,seq_len):
     """
     x: (B,T,E)
@@ -27,9 +31,14 @@ class MultiHeadAttention(nn.Module):
     return: (B,T,E)
     """
     return x.transpose(1,2).reshape(bs,seq_len,self.embed_dim)
+  def _causal_mask(self,seq_len):
+    """
+    return: (T,T)
+    """
+    mask = torch.ones((seq_len,seq_len),dtype=torch.bool,device=device)
+    return torch.tril(mask)
   def forward(self,x):
-    print(x.shape)
-    bs, seq_len, emb = x.shape
+    bs, seq_len, _ = x.shape
 
     # (B,H,T,D)
     q = self._split_heads(self.q_proj(x),bs,seq_len)
@@ -39,13 +48,17 @@ class MultiHeadAttention(nn.Module):
     scale = self.head_dim ** 0.5
     attn_scores = torch.matmul(q,k.transpose(-1,-2)) * scale # (B,H,T,T)
 
+    if self.causal:
+      causal_mask = self._causal_mask(seq_len) # (T,T)
+      causal_mask = causal_mask.unsqueeze(0).unsqueeze(0)
+      attn_scores = attn_scores.masked_fill(~causal_mask,float('-inf'))
+
     attn_weights = F.softmax(attn_scores,dim=-1)
     attn_out = torch.matmul(attn_weights,v) # (B,H,T,D)
     return self.out_proj(self._merge_heads(attn_out,bs,seq_len))
 
 if __name__ == "__main__":
-  device = torch.accelerator.current_accelerator().type
   print(f"***  using {device}")
-  attn = MultiHeadAttention(512, 8).to(device)
-  X = torch.rand(size=(2, 10, 512)).to(device)
+  attn = MultiHeadAttention(256, 4, causal=True).to(device)
+  X = torch.rand(size=(2, 10, 256)).to(device)
   attn.forward(X)
